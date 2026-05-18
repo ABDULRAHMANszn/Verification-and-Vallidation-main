169258c9e4391dbce163d6ebee652c961fe8e88f
Abdulrahman <youremail@example.com>
2026-05-18 16:22:12 +0300

    sami4

--- tests/aboodtest.py
+++ tests/aboodtest.py
@@ -2,6 +2,7 @@ from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.edge.service import Service
 from webdriver_manager.microsoft import EdgeChromiumDriverManager
+import time
 
 driver = webdriver.Edge(service=Service(EdgeChromiumDriverManager().install()))
 driver.implicitly_wait(5)
@@ -11,3 +12,14 @@ driver.maximize_window()
 driver.get("http://localhost:3000/")
 
 
+sign_up_btn = driver.find_element(By.XPATH, '//a[contains(text(),"Sign Up")]')
+driver.execute_script("arguments[0].click();", sign_up_btn)
+driver.find_element(By.ID, "username").send_keys("sami2")
+driver.find_element(By.ID, "password").send_keys("123456")
+driver.find_element(By.ID, "email").send_keys("sass@gmail.com")
+driver.find_element(By.ID, "phone").send_keys("1234567890")
+driver.find_element(By.ID, "address").send_keys("123 Main St")
+driver.find_element(By.ID, "register-btn").click()
+
+input("Press Enter to close...")  # ← browser stays open until you press Enter
+driver.quit()
