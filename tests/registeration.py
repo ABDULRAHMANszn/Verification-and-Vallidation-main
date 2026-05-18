f273ede666a616a150ece0efbd20e38cb416b201
EnADEL9 <adilalrys7@gmail.com>
2026-05-18 22:34:59 +0300

    42 test is done

--- tests/registeration.py
+++ tests/registeration.py
@@ -2,6 +2,8 @@ from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.edge.service import Service
 from webdriver_manager.microsoft import EdgeChromiumDriverManager
+import time
+
 
 driver = webdriver.Edge(service=Service(EdgeChromiumDriverManager().install()))
 driver.implicitly_wait(5)
@@ -17,6 +19,71 @@ def open_signup():
 #################################
 # Registeration
 #################################
+
+
+# Registration with empty username
+# try:
+#     driver.get("http://localhost:3000/login?mode=signup") 
+#     time.sleep(3)
+#     print(driver.current_url)
+
+#     driver.find_element(By.ID, "username").send_keys("")
+#     driver.find_element(By.ID, "password").send_keys("123456")
+#     driver.find_element(By.ID, "email").send_keys("sass3@gmail.com")
+#     driver.find_element(By.ID, "phone").send_keys("123456789")
+#     driver.find_element(By.ID, "address").send_keys("IYV Istanbul Turkey")
+#     driver.find_element(By.ID, "register-btn").click()
+
+#     assert "/login?mode=signup" not in driver.current_url
+#     print("TC-003 PASSED - Username with empty username accepted")
+
+# except AssertionError:
+#     print("TC-003 FAILED - Username with less than 3 chars was accepted")
+
+# driver.get("http://localhost:3000/")
+
+
+# # Registration with empty phone
+# try:
+#     driver.get("http://localhost:3000/login?mode=signup") 
+#     time.sleep(3)
+#     print(driver.current_url)
+
+#     driver.find_element(By.ID, "username").send_keys("ibrahim")
+#     driver.find_element(By.ID, "password").send_keys("123456")
+#     driver.find_element(By.ID, "email").send_keys("sass3@gmail.com")
+#     driver.find_element(By.ID, "phone").send_keys("")
+#     driver.find_element(By.ID, "address").send_keys("IYV Istanbul Turkey")
+#     driver.find_element(By.ID, "register-btn").click()
+
+#     assert "/login?mode=signup" not in driver.current_url
+#     print("TC-003 PASSED - Username with empty username accepted")
+
+# except AssertionError:
+#     print("TC-003 FAILED - Username with less than 3 chars was accepted")
+
+# driver.get("http://localhost:3000/")
+
+# # Registration with empty address
+# try:
+#     driver.get("http://localhost:3000/login?mode=signup") 
+#     time.sleep(3)
+#     print(driver.current_url)
+
+#     driver.find_element(By.ID, "username").send_keys("mustafa")
+#     driver.find_element(By.ID, "password").send_keys("123456")
+#     driver.find_element(By.ID, "email").send_keys("sass3@gmail.com")
+#     driver.find_element(By.ID, "phone").send_keys("123456789")
+#     driver.find_element(By.ID, "address").send_keys("")
+#     driver.find_element(By.ID, "register-btn").click()
+
+#     assert "/login?mode=signup" not in driver.current_url
+#     print("TC-003 PASSED - Username with empty username accepted")
+
+# except AssertionError:
+#     print("TC-003 FAILED - Username with less than 3 chars was accepted")
+
+# driver.get("http://localhost:3000/")
 
 # TC-003 Username with less than 3 chars
 # try:
@@ -125,26 +192,256 @@ def open_signup():
 # #########################################
 
 # navigate from sign in to sign up page
-try:
-    driver.get("http://localhost:3000/login")
+# try:
+#     driver.get("http://localhost:3000/login") 
+#     time.sleep(3)
+#     print(driver.current_url)
 
-    signup_button = wait.until(EC.element_to_be_clickable((By.XPATH, '//h1[contains(text(),"Sign Up")]'))).click()
+#     signup_button = driver.find_element(By.ID,'toggle-login-btn')
+#     driver.execute_script("arguments[0].click();", signup_button)
+#     time.sleep(2)
+#     assert "/login?mode=signup" not in driver.current_url
+#     print("TC-003 PASSED - navigating rejected")
+
+# except AssertionError:
+#     print("TC-003 FAILED - navigating accepted")
+
+
+
+# navigation from registration to login
+# try:
+#     driver.get("http://localhost:3000/login?mode=signup") 
+#     time.sleep(3)
+#     print(driver.current_url)
+
+#     signup_button = driver.find_element(By.ID,'toggle-login-btn')
+#     driver.execute_script("arguments[0].click();", signup_button)
+#     time.sleep(2)
+#     assert "/login" not in driver.current_url
+#     print("TC-003 PASSED - navigating rejected")
+
+# except AssertionError:
+#     print("TC-003 FAILED - navigating accepted")
     
-    create_account_text = driver.find_element(By.XPATH, '//p[contains(text(), "Create Account")]')
+    
+# Login and then Logout
+# try:
+#     driver.get("http://localhost:3000/login") 
+#     time.sleep(2)
+#     print(driver.current_url)
 
-    assert "/login" not in driver.current_url
-    print("TC-003 PASSED - navigating rejected")
+#     driver.find_element(By.ID, "username").send_keys("sami2")
+#     driver.find_element(By.ID, "password").send_keys("123456")
+#     driver.find_element(By.ID, 'login-btn').click()
+#     time.sleep(2)
+#     assert "/login" not in driver.current_url
+    
+#     driver.find_element(By.ID, 'logout-btn').click()
+    
+#     assert "/login" not in driver.current_url
+#     print("0000 - logout successfully")
 
-except AssertionError:
-    print("TC-003 FAILED - navigating accepted")
-
-
+# except AssertionError:
+#     print("0000 - logout failed")
 
 
 
 # #########################################
 # # Meals
 # #########################################
+
+# Add meal to cart
+# try:
+#     driver.get("http://localhost:3000")
+#     time.sleep(2)
+#     meal1 = driver.find_element(By.ID, 'add-to-cart-1')
+#     driver.execute_script("arguments[0].click();", meal1)
+#     time.sleep(1)
+#     cart = driver.find_element(By.ID, "cart-btn").click()
+#     time.sleep(1)
+#     total = driver.find_element(By.ID, "cart-total-price").text
+#     price = int(total.replace("₺",""))
+
+#     assert price == 120
+#     print("00000 - Meal 1 added to cart")
+# except AssertionError:
+#     print("00000 - Meal 1 not added to cart")
+
+
+# driver.execute_script("window.localStorage.clear();")
+# driver.get("http://localhost:3000/")
+# driver.quit()
+
+
+# Add same meal multiple times to cart
+# try:
+#     driver.get("http://localhost:3000")
+#     time.sleep(2)
+#     meal1 = driver.find_element(By.ID, 'add-to-cart-1')
+#     driver.execute_script("arguments[0].click();", meal1)
+#     driver.execute_script("arguments[0].click();", meal1)
+#     driver.execute_script("arguments[0].click();", meal1)
+#     time.sleep(1)
+#     cart = driver.find_element(By.ID, "cart-btn").click()
+#     time.sleep(1)
+#     total = driver.find_element(By.ID, "cart-total-price").text
+#     price = int(total.replace("₺",""))
+
+#     assert price == 360
+#     print("00000 - Meal 1 added multiple times to cart")
+# except AssertionError:
+#     print("00000 - Meal 1 not added multiple times to cart")
+
+
+# driver.execute_script("window.localStorage.clear();")
+# driver.get("http://localhost:3000/")
+# driver.quit()
+
+
+
+# Removeing meal from a cart
+# driver.get("http://localhost:3000")
+# time.sleep(2)
+# addbtu = driver.find_element(By.ID, 'add-to-cart-1')
+# driver.execute_script("arguments[0].click();", addbtu)
+# driver.execute_script("arguments[0].click();", addbtu)
+# driver.execute_script("arguments[0].click();", addbtu)
+# driver.execute_script("arguments[0].click();", addbtu)
+# driver.execute_script("arguments[0].click();", addbtu)
+# driver.find_element(By.ID, 'cart-btn').click()
+# removebut = driver.find_element(By.ID, 'decrease-1')
+# driver.execute_script("arguments[0].click();", removebut)
+
+# time.sleep(2)
+
+# total = driver.find_element(By.ID, "cart-total-price").text
+# price = int(total.replace("₺",""))
+
+# assert price == 480
+# print("00000 - Removing one item correctly")
+
+# driver.quit()
+
+
+# Confirm button hidden when cart is empty
+# try:
+#     driver.get("http://localhost:3000/")
+#     time.sleep(2)
+
+#     cart_btn = driver.find_element(By.ID, "cart-btn")
+
+#     assert cart_btn.is_displayed() == False
+
+#     print("000000000 PASSED - Confirm button hidden when cart is empty")
+
+# except AssertionError:
+#     print("00000000000 FAILED - Confirm button is visible while cart is empty")
+
+
+# driver.execute_script("window.localStorage.clear();")
+# driver.get("http://localhost:3000/")
+
+
+# Confirm button visible when cart is not empty
+# try:
+#     driver.get("http://localhost:3000/")
+#     time.sleep(2)
+
+#     addbtu = driver.find_element(By.ID, 'add-to-cart-1')
+#     driver.execute_script("arguments[0].click();", addbtu)
+#     time.sleep(1)
+#     cart_btn = driver.find_element(By.ID, "cart-btn")
+
+#     assert cart_btn.is_displayed() == True
+
+#     print("000000000 PASSED - Confirm button visible when cart is not empty")
+
+# except AssertionError:
+#     print("00000000000 FAILED - Confirm button is visible while cart is not empty")
+
+
+# driver.execute_script("window.localStorage.clear();")
+# driver.get("http://localhost:3000/")
+
+# Open cart via nav button
+# try:
+#     driver.get("http://localhost:3000")
+#     time.sleep(1)
+
+#     openCart = driver.find_element(By.ID, 'nav-cart-btn')
+#     driver.execute_script("arguments[0].click();", openCart)
+#     time.sleep(1)
+#     cart_btn = driver.find_element(By.ID, "cart-btn")
+
+#     assert cart_btn.is_displayed() == True
+
+#     print("000000000 PASSED - Cart model is opened via button in nav")
+
+# except AssertionError:
+#     print("00000000000 FAILED - Cart model is not opened via button in nav")
+
+
+# driver.execute_script("window.localStorage.clear();")
+# driver.get("http://localhost:3000/")
+
+
+
+# Open cart via nav button
+# try:
+#     driver.get("http://localhost:3000")
+#     time.sleep(1)
+
+#     addbtu = driver.find_element(By.ID, 'add-to-cart-3')
+#     driver.execute_script("arguments[0].click();", addbtu)
+#     openCart = driver.find_element(By.ID, 'nav-cart-btn')
+#     driver.execute_script("arguments[0].click();", openCart)
+#     time.sleep(1)
+#     cart_btn = driver.find_element(By.ID, "cart-btn")
+#     driver.execute_script("arguments[0].click();", cart_btn)
+
+#     driver.find_element(By.ID, 'increase-3').click()
+#     driver.find_element(By.ID, 'increase-3').click()
+    
+#     total = driver.find_element(By.ID, "cart-total-price").text
+#     price = int(total.replace("₺",""))
+
+#     assert price == 300
+#     print("000000000 PASSED - Increasing item quantity via cart modal is successful")
+
+# except AssertionError:
+#     print("00000000000 FAILED - Cart model is not opened via button in nav")
+
+
+# try:
+#     driver.get("http://localhost:3000")
+#     time.sleep(1)
+
+#     addbtu = driver.find_element(By.ID, 'add-to-cart-3')
+#     driver.execute_script("arguments[0].click();", addbtu)
+#     driver.execute_script("arguments[0].click();", addbtu)
+#     driver.execute_script("arguments[0].click();", addbtu)
+#     openCart = driver.find_element(By.ID, 'nav-cart-btn')
+#     driver.execute_script("arguments[0].click();", openCart)
+#     time.sleep(1)
+#     cart_btn = driver.find_element(By.ID, "cart-btn")
+#     driver.execute_script("arguments[0].click();", cart_btn)
+
+#     driver.find_element(By.ID, 'decrease-3').click()
+#     driver.find_element(By.ID, 'decrease-3').click()
+    
+#     total = driver.find_element(By.ID, "cart-total-price").text
+#     price = int(total.replace("₺",""))
+
+#     assert price == 100
+#     print("000000000 PASSED - Decreasing item quantity via cart modal is successful")
+
+# except AssertionError:
+#     print("00000000000 FAILED - Decreasing item quantity via cart modal is not successful")
+
+
+
+# driver.execute_script("window.localStorage.clear();")
+# driver.get("http://localhost:3000/")
 
 
 # ##########################################################
@@ -153,7 +450,7 @@ except AssertionError:
 
 # # TC-FUNC-015 — Create order with valid data
 # try:
-#     open_signup()
+#     driver.get("http://localhost:3000/")
 
 #     driver.find_element(By.ID, "add-to-cart-1").click()
 #     driver.find_element(By.ID, "cart-btn").click()
@@ -176,7 +473,7 @@ except AssertionError:
 
 # # TC-FUNC-016 — Create order with non-existent meal
 # try:
-#     open_signup()
+#     driver.get("http://localhost:3000/")
 
 #     driver.find_element(By.ID, "add-to-cart-9999").click()
 
@@ -229,7 +526,7 @@ except AssertionError:
 
 # # TC-FUNC-019 — Get orders for non-existent user
 # try:
-#     open_signup()
+#     driver.get("http://localhost:3000/")
 
 #     # simulate non-existent user
 #     driver.execute_script(
@@ -263,7 +560,7 @@ except AssertionError:
 
 # # TC-FUNC-020 — Create order with quantity = 0
 # try:
-#     open_signup()
+#     driver.get("http://localhost:3000/")
 
 #     driver.find_element(By.ID, "add-to-cart-1").click()
 
@@ -292,7 +589,7 @@ except AssertionError:
 
 # # TC-FUNC-021 — Create order with negative quantity
 # try:
-#     open_signup()
+#     driver.get("http://localhost:3000/")
 
 #     driver.find_element(By.ID, "add-to-cart-1").click()
 
@@ -320,7 +617,7 @@ except AssertionError:
 
 # # TC-FUNC-024 — Verify total price calculation
 # try:
-#     open_signup()
+#     driver.get("http://localhost:3000/")
 
 #     driver.find_element(By.ID, "add-to-cart-1").click()
 #     driver.find_element(By.ID, "add-to-cart-1").click()
@@ -348,3 +645,26 @@ except AssertionError:
 
 # driver.execute_script("window.localStorage.clear();")
 # driver.get("http://localhost:3000/")
+
+
+# Completely removing a meal
+# driver.get("http://localhost:3000")
+# time.sleep(3)
+# addbtu = driver.find_element(By.ID, 'add-to-cart-1')
+# driver.execute_script("arguments[0].click();", addbtu)
+# driver.execute_script("arguments[0].click();", addbtu)
+# driver.find_element(By.ID, 'cart-btn').click()
+# emovebut =driver.find_element(By.ID, 'decrease-1')
+# driver.execute_script("arguments[0].click();", emovebut)
+# driver.execute_script("arguments[0].click();", emovebut)
+
+# time.sleep(2)
+
+# total = driver.find_element(By.ID, "cart-total-price").text
+# price = int(total.replace("₺",""))
+
+# assert price == 0
+# print("00000 - completely removed correctly")
+
+# driver.quit()
+
